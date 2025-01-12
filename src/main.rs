45bd3f4e174d50eae748178eed76c6228e3a1721
One <43485962+c-git@users.noreply.github.com>
2025-01-12 18:49:03 -0500

    Make code closer to hand written code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use shuttle_runtime::tokio;
+use shuttle_runtime::{
+    IntoResource,
+    __internals::{self, serde_json},
+    tokio, ResourceFactory, ResourceInputBuilder,
+};
 
 struct CustomService;
 #[shuttle_runtime::async_trait]
@@ -10,38 +14,29 @@ impl shuttle_runtime::Service for CustomService {
 
 #[tokio::main]
 async fn main() {
-    ::shuttle_runtime::__internals::start(__loader, __runner).await;
+    __internals::start(__loader, runner).await;
 }
 
-async fn __loader(
-    factory: ::shuttle_runtime::ResourceFactory,
-) -> ::std::result::Result<
-    ::std::vec::Vec<::std::vec::Vec<::core::primitive::u8>>,
-    ::shuttle_runtime::Error,
-> {
-    use ::shuttle_runtime::ResourceInputBuilder;
+async fn __loader(factory: ResourceFactory) -> Result<Vec<Vec<u8>>, shuttle_runtime::Error> {
     let mut inputs = Vec::new();
     let input: <shuttle_shared_db::Postgres as ResourceInputBuilder>::Input =
         shuttle_shared_db::Postgres::default()
             .build(&factory)
             .await?;
-    let json = ::shuttle_runtime::__internals::serde_json::to_vec(&input).unwrap();
+    let json = serde_json::to_vec(&input).unwrap();
     inputs.push(json);
     Ok(inputs)
 }
 
-async fn __runner(
-    resources: ::std::vec::Vec<::std::vec::Vec<::core::primitive::u8>>,
-) -> Result<CustomService, shuttle_runtime::Error> {
-    use ::shuttle_runtime::{IntoResource, ResourceInputBuilder};
+async fn runner(resources: Vec<Vec<u8>>) -> Result<CustomService, shuttle_runtime::Error> {
     let mut iter = resources.into_iter();
     let x: <shuttle_shared_db::Postgres as ResourceInputBuilder>::Output =
-        ::shuttle_runtime::__internals::serde_json::from_slice(&iter.next().unwrap()).unwrap();
+        serde_json::from_slice(&iter.next().unwrap()).unwrap();
     let operator: shuttle_shared_db::SerdeJsonOperator = x.into_resource().await.unwrap();
-    __shuttle_main(operator).await
+    shuttle_main(operator).await
 }
 
-async fn __shuttle_main(
+async fn shuttle_main(
     operator: shuttle_shared_db::SerdeJsonOperator,
 ) -> Result<CustomService, shuttle_runtime::Error> {
     operator.0.check().await.unwrap();
