65851de903805179704de3f3d7522b0c58023cd9
One <43485962+c-git@users.noreply.github.com>
2025-01-12 19:28:38 -0500

    Remove code not needed to trigger

--- src/main.rs
+++ src/main.rs
@@ -31,36 +31,13 @@ async fn runner(resources: Vec<Vec<u8>>) -> Result<CustomService, shuttle_runtim
 }
 #[derive(Clone, Debug)]
 pub struct SerdeJsonOperator(pub opendal::Operator);
-impl SerdeJsonOperator {
-    pub async fn read_serialized<T: serde::de::DeserializeOwned>(
-        &self,
-        key: &str,
-    ) -> Result<T, opendal::Error> {
-        let bytes = self.0.read(key).await?;
-
-        serde_json::from_slice(&bytes.to_vec()).map_err(|e| {
-            opendal::Error::new(opendal::ErrorKind::Unexpected, "deserialization error")
-                .set_source(e)
-        })
-    }
-    pub async fn write_serialized<T: serde::Serialize>(
-        &self,
-        key: &str,
-        value: &T,
-    ) -> Result<(), opendal::Error> {
-        let b = serde_json::to_vec(value).map_err(|e| {
-            opendal::Error::new(opendal::ErrorKind::Unexpected, "serialization error").set_source(e)
-        })?;
-
-        self.0.write(key, b).await
-    }
-}
+impl SerdeJsonOperator {}
 
 #[shuttle_runtime::async_trait]
 impl IntoResource<SerdeJsonOperator>
     for <shuttle_shared_db::Postgres as ResourceInputBuilder>::Output
 {
     async fn into_resource(self) -> Result<SerdeJsonOperator, shuttle_runtime::Error> {
-        Ok(SerdeJsonOperator(self.into_resource().await?))
+        todo!()
     }
 }
