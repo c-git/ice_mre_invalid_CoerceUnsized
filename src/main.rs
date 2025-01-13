f949214b060528bdb974dedd43c2f6d83103216d
One <43485962+c-git@users.noreply.github.com>
2025-01-12 21:52:53 -0500

    No ICE: Remove passing loader

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,7 @@ impl shuttle_runtime::Service for CustomService {
 
 #[tokio::main]
 async fn main() {
-    start(loader, runner).await;
-}
-
-async fn loader(_factory: ResourceFactory) -> Result<Vec<Vec<u8>>, shuttle_runtime::Error> {
-    todo!()
+    start(runner).await;
 }
 
 async fn runner(_resources: Vec<Vec<u8>>) -> Result<CustomService, shuttle_runtime::Error> {
