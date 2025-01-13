3fc8df258fbb14d5016e9b8382f2a35d80751368
One <43485962+c-git@users.noreply.github.com>
2025-01-13 02:02:33 -0500

    ICE: Drop runtime import

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use shuttle_runtime::{
-    runtime::*,
     IntoResource,
     __internals::serde_json,
     async_trait, http,
     tokio::{self},
-    ReceiverStream, ResourceInputBuilder,
+    ResourceInputBuilder,
 };
 use std::{
     future::Future,
     sync::{Arc, Mutex},
 };
-use tonic::{self, server::NamedService, transport::Server, Request, Response, Status};
+use tonic::{self, server::NamedService, transport::Server};
 
 #[tokio::main]
 async fn main() {
@@ -65,42 +64,7 @@ impl<R> Alpha<R> {
 }
 
 #[async_trait]
-impl<R> Runtime for Alpha<R>
-where
-    R: Runner + Send + 'static,
-{
-    async fn load(&self, _request: Request<LoadRequest>) -> Result<Response<LoadResponse>, Status> {
-        todo!()
-    }
-
-    async fn start(
-        &self,
-        _request: Request<StartRequest>,
-    ) -> Result<Response<StartResponse>, Status> {
-        todo!()
-    }
-
-    async fn stop(&self, _request: Request<StopRequest>) -> Result<Response<StopResponse>, Status> {
-        todo!()
-    }
-
-    type SubscribeStopStream = ReceiverStream<Result<SubscribeStopResponse, Status>>;
-
-    async fn subscribe_stop(
-        &self,
-        _request: Request<SubscribeStopRequest>,
-    ) -> Result<Response<Self::SubscribeStopStream>, Status> {
-        todo!()
-    }
-
-    async fn version(&self, _request: Request<Ping>) -> Result<Response<VersionInfo>, Status> {
-        todo!()
-    }
-
-    async fn health_check(&self, _request: Request<Ping>) -> Result<Response<Pong>, Status> {
-        todo!()
-    }
-}
+impl<R> Runtime for Alpha<R> where R: Runner + Send + 'static {}
 
 #[async_trait]
 pub trait Runner {}
@@ -114,41 +78,7 @@ where
 }
 
 #[async_trait]
-pub trait Runtime: Send + Sync + 'static {
-    /// Load a service file to be ready to start it
-    async fn load(
-        &self,
-        request: tonic::Request<LoadRequest>,
-    ) -> std::result::Result<tonic::Response<LoadResponse>, tonic::Status>;
-    /// Start a loaded service file
-    async fn start(
-        &self,
-        request: tonic::Request<StartRequest>,
-    ) -> std::result::Result<tonic::Response<StartResponse>, tonic::Status>;
-    /// Stop a started service
-    async fn stop(
-        &self,
-        request: tonic::Request<StopRequest>,
-    ) -> std::result::Result<tonic::Response<StopResponse>, tonic::Status>;
-    /// Server streaming response type for the SubscribeStop method.
-    type SubscribeStopStream: tonic::codegen::tokio_stream::Stream<
-            Item = std::result::Result<SubscribeStopResponse, tonic::Status>,
-        > + Send
-        + 'static;
-    /// Channel to notify a service has been stopped
-    async fn subscribe_stop(
-        &self,
-        request: tonic::Request<SubscribeStopRequest>,
-    ) -> std::result::Result<tonic::Response<Self::SubscribeStopStream>, tonic::Status>;
-    async fn version(
-        &self,
-        request: tonic::Request<Ping>,
-    ) -> std::result::Result<tonic::Response<VersionInfo>, tonic::Status>;
-    async fn health_check(
-        &self,
-        request: tonic::Request<Ping>,
-    ) -> std::result::Result<tonic::Response<Pong>, tonic::Status>;
-}
+pub trait Runtime: Send + Sync + 'static {}
 
 pub struct RuntimeServer<T: Runtime> {
     inner: Arc<T>,
@@ -194,10 +124,10 @@ where
             "" => {
                 #[allow(non_camel_case_types, dead_code)]
                 struct LoadSvc<T: Runtime>(pub Arc<T>);
-                impl<T: Runtime> tonic::server::UnaryService<LoadRequest> for LoadSvc<T> {
-                    type Response = LoadResponse;
+                impl<T: Runtime> tonic::server::UnaryService<()> for LoadSvc<T> {
+                    type Response = ();
                     type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
-                    fn call(&mut self, _request: tonic::Request<LoadRequest>) -> Self::Future {
+                    fn call(&mut self, _request: tonic::Request<()>) -> Self::Future {
                         todo!()
                     }
                 }
